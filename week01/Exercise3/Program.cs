8f253640d6eaeabe06ca4971c80aaf352c1c110b
eros-baez <baezerosdaniel@gmail.com>
2026-01-15 14:51:28 -0300

    w01 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 101);
+        int guess = 0;
+
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber == guess)
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
