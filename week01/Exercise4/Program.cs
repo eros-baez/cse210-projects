1c3e533850189cf56b709cf899d92a75f3f67b0e
eros-baez <baezerosdaniel@gmail.com>
2026-01-15 19:48:06 -0300

    w01 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        Console.WriteLine($"The list is: {String.Join(", ", numbers)}");
+
+////////////////////////////////////////////////////////////////////
+
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+
+////////////////////////////////////////////////////////////////////
+
+        if (numbers.Count > 0)
+        {
+            double averageNumber = (double) sum / (numbers.Count);
+            Console.WriteLine($"The average is: {averageNumber}");
+        }
+
+        else
+        {
+            Console.WriteLine("No number was found.");
+        }
+
+////////////////////////////////////////////////////////////////////
+
+        int larger = 0;
+        foreach (int n in numbers)
+        {
+            if (n > larger)
+            {
+                larger = n;
+            }
+        }
+        Console.WriteLine($"The largest number is: {larger}");
+
+        int smaller = numbers[0];
+        foreach (int m in numbers)
+        {
+            if (m < smaller)
+            {
+                smaller = m;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smaller}");
     }
 }
