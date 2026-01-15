a5a236d7e4b33325633f36bdc07c5048e4ce5ea5
eros-baez <baezerosdaniel@gmail.com>
2026-01-14 22:56:44 -0300

    w01 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade < 70)
+        {
+            Console.WriteLine("Sorry, you did not pass the course. Keep trying, you can do it!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations!, You have passed this course. Good luck for next course!");
+        }
     }
 }
