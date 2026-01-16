abe82ce1bd712917a701f7968f0b34150f3343aa
eros-baez <baezerosdaniel@gmail.com>
2026-01-15 22:28:49 -0300

    w01 exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void WelcomeMessage ()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        string DisplayUserName (string userName)
+        {
+            return $"Hello {userName}.";
+        }
+
+        string DisplayNumber (int userNumber)
+        {
+            return $"Your favorite number is: {userNumber}";
+        }
+
+        string DisplaySquaredNumber (int userNumber, string userName)
+        {
+            double squared = Math.Pow(userNumber, 2);
+
+            return $"Brother {userName}, the square of your number is: {squared}";
+        }
+
+        WelcomeMessage();
+
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+////////////////////////////////////////////////////////////////////////////////
+ 
+        Console.WriteLine(DisplayUserName (userName));
+        Console.WriteLine(DisplayNumber (userNumber));
+        Console.WriteLine(DisplaySquaredNumber(userNumber, userName));
+
     }
 }
